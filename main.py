23dc1a8a8c50e289980d1e319a5c5d9f5f9a61cf
Yahui XU <74300759+Aya1102@users.noreply.github.com>
2021-12-07 16:50:46 +0000

    add pandas

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas
 
+data = {
+  'series1':[1,3,4,6,7]
+  'series2':[15,46,78,89]
+}
+
+df = pandas.DataFrame(data)
 st.title('Our first streamlist App')
 st.subheader('Automate Everything with Python')
 st.write('''Now add some text
 Hope you like it
 And enjoy''')
+st.write(df)
